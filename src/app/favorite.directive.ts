2bee313c131e89a48e846552dd8b460eec8d03e4
Paul <paul@beambient.co.uk>
2021-01-29 10:23:15 +0000

    end of 03_04b

---
+++ src/app/favorite.directive.ts
@@ -0,0 +1,10 @@
+import { Directive, HostBinding } from '@angular/core';
+
+ @Directive({
+   selector: '[mwFavorite]'
+  })
+ export class FavoriteDirective {
+   @HostBinding('class.is-favorite') isFavorite = true;
+
+   constructor() { }
+ }
