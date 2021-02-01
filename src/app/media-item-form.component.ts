9103831a0b82c949248d2256d0d53e43172df59e
Paul <paul@beambient.co.uk>
2021-02-01 10:00:42 +0000

    end of 04_04b

--- src/app/media-item-form.component.ts
+++ src/app/media-item-form.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'mw-media-item-form',
-  templateUrl: './media-item-form.component.html',
-  styleUrls: ['./media-item-form.component.css']
+  selector: "mw-media-item-form",
+  templateUrl: "./media-item-form.component.html",
+  styleUrls: ["./media-item-form.component.css"],
 })
 export class MediaItemFormComponent implements OnInit {
   form: FormGroup;
 
   ngOnInit() {
     this.form = new FormGroup({
-      medium: new FormControl('Movies'),
-      name: new FormControl(''),
-      category: new FormControl(''),
-      year: new FormControl(''),
+      medium: new FormControl("Movies"),
+      name: new FormControl(
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.pattern("[\\w\\-\\s\\/]+"),
+        ])
+      ),
+      category: new FormControl(""),
+      year: new FormControl(""),
     });
   }
 
